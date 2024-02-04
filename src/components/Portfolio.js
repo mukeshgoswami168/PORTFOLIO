29a549b92bf099dd590e931e87c4d0d3eb2d9638
mukeshgoswami168 <mukeshgoswami168@gmail.com>
2024-02-05 01:26:10 +0530

    third change

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,6 @@
 import study from "../assets/study.jpg"
 import blog from "../assets/blog.jpg"
-import zappier from "../assets/zappier.png"
+import shop from "../assets/shop.jpg"
 
 function Portfolio(){
     return(
@@ -13,24 +13,24 @@ function Portfolio(){
             </div>
             <div className="  grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
                 <div className="   shadow-md shadow-gray-600  rounded-lg">
-                    <img src={study}  alt="" className="rounded-md w-[100%] duration-500 hover:scale-105"></img>
+                    <img src={study}  alt="" className="rounded-md w-[100%] h-[245px] duration-500 hover:scale-105"></img>
                     <div className=" flex items-center justify-center ">
-                        <button className="w-1/2  font-semibold px-6 py-3 mt-[55px] m-4 duration-200 hover:scale-105"><a href="https://study-new-hosting.vercel.app/" target="-blank">Demo</a> </button>
-                        <button className="w-1/2 px-6 py-3 font-semibold mt-[55px] m-4 duration-200 hover:scale-105"><a href="https://github.com/mukeshgoswami168/StudyNew-Hosting" target="-blank">Code Here</a></button>
+                        <button className="w-1/2  font-semibold px-6 py-3  m-4 duration-200 hover:scale-105"><a href="https://study-new-hosting.vercel.app/" target="_blank">Demo</a> </button>
+                        <button className="w-1/2 px-6 py-3 font-semibold  m-4 duration-200 hover:scale-105"><a href="https://github.com/mukeshgoswami168/StudyNew-Hosting" target="_blank">Code Here</a></button>
                     </div>
                 </div>
                 <div className="   shadow-md shadow-gray-600  rounded-lg">
                     <img src={blog}  alt="" className="rounded-md w-[100%] duration-500 hover:scale-105"></img>
                     <div className=" flex items-center justify-center ">
                         <button className="w-1/2 font-semibold px-6 py-3 m-4 duration-200 hover:scale-105"><a href="" target="-blank">Demo</a> </button>
-                        <button className="w-1/2 font-semibold px-6 py-3 m-4 duration-200 hover:scale-105"><a href="https://github.com/mukeshgoswami168/MUKESH-BLOGS" target="-blank">Code Here</a></button>
+                        <button className="w-1/2 font-semibold px-6 py-3 m-4 duration-200 hover:scale-105"><a href="https://github.com/mukeshgoswami168/MUKESH-BLOGS" target="_blank">Code Here</a></button>
                     </div>
                 </div>
                 <div className="   shadow-md shadow-gray-600  rounded-lg">
-                    <img src={zappier}  alt="" className="rounded-md w-[100%] duration-500 hover:scale-105"></img>
+                    <img src={shop}  alt="" className="rounded-md w-[100%] duration-500 h-[245px] hover:scale-105"></img>
                     <div className=" flex items-center justify-center ">
-                        <button className="w-1/2 font-semibold px-6 py-3 m-4 duration-200 hover:scale-105">Demo </button>
-                        <button className="w-1/2 font-semibold px-6 py-3 m-4 duration-200 hover:scale-105"><a  href="https://github.com/mukeshgoswami168/Billing-Automation" target="-blank">Code Here</a></button>
+                        <button className="w-1/2 font-semibold px-6 py-3 m-4 duration-200 hover:scale-105"><a href="https://shopping-app-lovat.vercel.app/" target="_blank">Demo</a> </button>
+                        <button className="w-1/2 font-semibold px-6 py-3 m-4 duration-200 hover:scale-105"><a  href="https://github.com/mukeshgoswami168/Shopping-App" target="_blank">Code Here</a></button>
                     </div>
                 </div>
             </div>
